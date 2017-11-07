a2a8885eaf1ea13533e80e328ee5a6b104c89000
Meezio Team <dev@meez.io>
2017-11-07 01:42:22 +0100

    left join Mongo

--- apicore/databases/nosql.py
+++ apicore/databases/nosql.py
@@ -36,6 +36,54 @@ class Db:
             raise Http404Exception()
 
     @classmethod
+    def getleftJoin(cls, collection, identifier, rightField, key="_id", projects=[]):
+        data = cls._leftJoin(collection, identifier, rightField, key, projects)
+        if data:
+            data = data.pop()
+            if "_id" in data:
+                data["uuid"] = data.pop("_id")
+            return data
+        else:
+            raise Http404Exception()
+
+    @classmethod
+    def getManyleftJoin(cls, collection, rightField, offset=0, limit=0, sort=None, projects=[]):
+        pass
+
+    @classmethod
+    def getleftJoinMany(cls, collection, identifier, rightField, key="_id", projects=[]):
+        pass
+
+    @classmethod
+    def getManyleftJoinMany(cls, collection, offset=0, limit=0, sort=None, projects=[]):
+        pass
+
+    @classmethod
+    def _leftJoin(cls, collection, identifier, rightField, key="_id", projects=[]):
+        if key == "_id":
+            try:
+                identifier = ObjectId(identifier)
+            except:
+                raise Http400Exception()
+
+        pipeline = [
+            {"$match": {key: identifier}},
+            {"$lookup": {"from": rightField, "localField": rightField, "foreignField": "_id", "as": rightField}}
+        ]
+        for project in projects:
+            pipeline.append({"$project": project})
+        print(pipeline)
+
+        data = list(cls.db[collection].aggregate(pipeline))
+
+        if data:
+            if "_id" in data:
+                data["uuid"] = data.pop("_id")
+            return data
+        else:
+            raise Http404Exception()
+
+    @classmethod
     def post(cls, data, collection):
         try:
             return cls.db[collection].insert_one(data).inserted_id
@@ -46,21 +94,15 @@ class Db:
     def patch(cls, data, collection, identifier, key="_id"):
         if key == "_id":
             try:
-                value = ObjectId(identifier)
+                identifier = ObjectId(identifier)
             except:
                 raise Http400Exception()
 
-        cls.db[collection].update_one({key: value}, {'$set': data})
+        cls.db[collection].update_one({key: identifier}, {'$set': data})
 
     @classmethod
-    def patchMany(cls, data, collection, identifier, key="_id"):
-        if key == "_id":
-            try:
-                value = ObjectId(identifier)
-            except:
-                raise Http400Exception()
-
-        cls.db[collection].update_many({key: value}, {'$set': data})
+    def patchMany(cls, data, collection, mongofilter):
+        cls.db[collection].update_many(mongofilter, {'$set': data})
 
     @classmethod
     def put(cls, data, collection, identifier, key="_id"):
@@ -70,7 +112,7 @@ class Db:
             except:
                 raise Http400Exception()
 
-        resu = cls.db[collection].replace_one({key: identifier}, data, True)
+        cls.db[collection].replace_one({key: identifier}, data, True)
 
     @classmethod
     def delete(cls, collection, identifier, key="_id"):
