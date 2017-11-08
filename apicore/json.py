70a496e9228815612fea93738cdb40975bcc23f5
Meezio Team <dev@meez.io>
2017-11-08 18:15:30 +0100

    Add datetime json converter

--- apicore/json.py
+++ apicore/json.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 from bson import objectid
 
 
@@ -10,5 +11,7 @@ class genericJsonEncoder(json.JSONEncoder):
     def default(self, data):
         if isinstance(data, objectid.ObjectId):
             return str(data)
+        elif isinstance(data, datetime.datetime):
+            return data.strftime("%Y-%m-%dT%H:%MZ")
         else:
             return json.JSONEncoder.default(self, data)
